0cb9d7b7c8a112840a9708a0897b72d8abf5648d
Abhijith Pradeep <abhijithprdp@gmail.com>
2021-01-10 15:08:13 +0530

    First Version

---
+++ src/users.js
@@ -0,0 +1,17 @@
+const users = 
+[
+    {
+        id: "0",
+        username: "admin",
+        email: "admim@mail.com",
+        password: "12345"
+    },
+    {
+        id: "1",
+        username: "admin1",
+        email: "admin1@mail.com",
+        password: "abcd"
+    }
+];
+
+module.exports = users;
